d400241183c07ac5d2ff9edabfc6bbaff268483f
Ivan Miragaya <MiragayaIvan@hotmail.com>
2021-05-29 03:48:06 -0300

    fix: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
-import { Box, Button, Grid, Slide, Typography } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Collapse,
+  Grid,
+  Slide,
+  Typography,
+} from "@material-ui/core";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 
 const HeaderTypography = withStyles((theme) => ({
@@ -18,16 +25,16 @@ const useStyles = makeStyles((theme) => ({
     minHeight: "106vh",
   },
   welcomeText: {
-    padding: "0 0 5rem 0",
+    padding: "0 0 20rem 0",
     textAlign: "center",
     fontSize: "1rem",
   },
   headerButton: {
     border: "2px solid #fff",
     borderRadius: "0%",
+    marginTop: "1rem",
     transition: "0.5s",
     color: "#fff",
-    marginTop: "7rem",
     "&:hover": {
       backgroundColor: "#ff9100",
       color: "#fff",
@@ -62,36 +69,46 @@ export default function Header(props) {
     <Grid container className={classes.root} ref={headerSection}>
       <Grid
         container
-        className={classes.welcomeText}
-        alignItems="center"
         justify="center"
-        direction="column"
+        alignItems="center"
+        className={classes.welcomeText}
       >
-        <Slide direction="left" in={checked} {...{ timeout: 1000 }}>
-          <HeaderTypography variant="p" component="h1">
-            <span className={classes.colorText}>Iván Miragaya.</span>
-            <br />
-            Full-Stack developer.
-          </HeaderTypography>
-        </Slide>
-        <Slide direction="right" in={checked} {...{ timeout: 1000 }}>
-          <Box>
-            <Button
-              onMouseEnter={() => setRotate(true)}
-              onMouseLeave={() => setRotate(false)}
-              size="large"
-              href="#about"
-              variant="outlined"
-              onClick={gotoAbout}
-              classes={{ root: classes.headerButton }}
-            >
-              Mis trabajos
-              <ArrowForwardIcon
-                className={rotate ? classes.iconAnim : classes.icon}
-              />
-            </Button>{" "}
-          </Box>
-        </Slide>
+        <HeaderTypography variant="p" component="h1">
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Slide direction="down" in={checked} {...{ timeout: 800 }}>
+              <Box>
+                <span className={classes.colorText}>Iván Miragaya.</span>{" "}
+              </Box>
+            </Slide>
+            <Slide direction="right" in={checked} {...{ timeout: 1600 }}>
+              <Box>Full-Stack developer. </Box>
+            </Slide>
+
+            <Slide direction="up" in={checked} {...{ timeout: 2000 }}>
+              <Box>
+                <Button
+                  onMouseEnter={() => setRotate(true)}
+                  onMouseLeave={() => setRotate(false)}
+                  size="large"
+                  href="#about"
+                  variant="outlined"
+                  onClick={gotoAbout}
+                  classes={{ root: classes.headerButton }}
+                >
+                  Mis trabajos
+                  <ArrowForwardIcon
+                    className={rotate ? classes.iconAnim : classes.icon}
+                  />
+                </Button>{" "}
+              </Box>
+            </Slide>
+          </Grid>
+        </HeaderTypography>
       </Grid>
     </Grid>
   );
